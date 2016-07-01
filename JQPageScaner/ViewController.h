80af973c72ea3a54ef1a4738760f967bbdcfed87
ljq <405164649@qq.com>
2016-07-01 22:08:45 +0800

    Initial Commit

---
+++ JQPageScaner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JQPageScaner
+//
+//  Created by lvjiaqi on 16/7/1.
+//  Copyright © 2016年 lvjiaqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
